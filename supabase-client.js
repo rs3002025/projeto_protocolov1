8c2cfa6b411e3fa5a1c37d3152f13ce7fd0e5f3f
rs3002025 <renatosampaio300@gmail.com>
2025-08-24 16:37:18 -0300

    Create supabase-client.js

---
+++ supabase-client.js
@@ -0,0 +1,9 @@
+// supabase-client.js
+const { createClient } = require('@supabase/supabase-js');
+
+const supabaseUrl = process.env.SUPABASE_URL;
+const supabaseKey = process.env.SUPABASE_SERVICE_KEY;
+
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+module.exports = supabase;
