bcd236122c6cb4fabf66877c9daa38c670f4f0a3
rs3002025 <renatosampaio300@gmail.com>
2026-09-01 09:12:50 -0300

    Update QR code URL construction with organization slug

--- static/script.js
+++ static/script.js
@@ -752,7 +752,7 @@ window.previsualizarPDF = async function(id = null, isFromForm = false) {
       qrcodeContainer.innerHTML = '';
       const numeroParts = protocolo.numero.split('/');
       if (numeroParts.length === 2) {
-          const urlConsulta = `${window.location.origin}/consulta/${numeroParts[1]}/${numeroParts[0]}`;
+          const urlConsulta = `${window.location.origin}/consulta/${encodeURIComponent(protocolo.organizacao_slug)}/${numeroParts[1]}/${numeroParts[0]}`;
           new QRCode(qrcodeContainer, { text: urlConsulta, width: 90, height: 90, correctLevel: QRCode.CorrectLevel.H });
       }
   }
